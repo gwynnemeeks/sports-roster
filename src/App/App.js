c3b0a84ee9378e70bda280bb5b82ccb2d1878a9b
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-08 14:01:55 -0500

    setup complete and looking good

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Sports Roster</h2>
+        <button className="btn btn-light"><i class="fas fa-swimmer">
+          </i>
+          </button>
+      </div>
+    );
+  }
+}
+
+export default App;
