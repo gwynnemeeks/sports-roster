4a97265bbd98aea3ee0998fe7f79ab17f6a1b9cd
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-21 17:21:03 -0500

    wip

---
+++ src/helpers/data/playersData.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+import utils from '../utils';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getPlayersByTeamId = (teamId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/players.json?orderBy="teamId"&equalTo="${teamId}"`)
+    .then(({ data }) => resolve(utils.convertFirebaseCollection(data)))
+    .catch((err) => reject(err));
+});
+
+export default { getPlayersByTeamId };
